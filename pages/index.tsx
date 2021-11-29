88fe20b256bff0b4216c8e31951da69f9c794551
Sejin-P <chunpark37@gmail.com>
2021-11-29 15:38:01 +0900

    apply index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import Head from 'next/head'
 import {Layout} from "../components/layout";
 import {CMS_NAME} from "../lib/constants";
 import {BaseContainer} from "../components/baseContainer";
+import Intro from "../components/intro";
 
-export default function Index({ allPosts }) {
-  const firstPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+const Index = () =>  {
   return (
       <>
         <Layout>
@@ -14,19 +13,10 @@ export default function Index({ allPosts }) {
           </Head>
           <BaseContainer>
             <Intro />
-            {firstPost && (
-                <HeroPost
-                    title={firstPost.title}
-                    coverImage={firstPost.coverImage}
-                    date={firstPost.date}
-                    author={firstPost.author}
-                    slug={firstPost.slug}
-                    excerpt={firstPost.excerpt}
-                />
-            )}
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
           </BaseContainer>
         </Layout>
       </>
   )
 }
+
+export default Index
