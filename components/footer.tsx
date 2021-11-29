17e7f93ad076d9b571b461f876f0187fd19c01f4
Sejin-P <chunpark37@gmail.com>
2021-11-29 17:48:18 +0900

    Apply justify end to place button right end

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,10 @@ export const Footer = () => {
                     <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
                         Statically Generated with Next.js.
                     </h3>
-                    <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+                    <div className="flex flex-col lg:flex-row justify-end items-center lg:pl-4 lg:w-1/2">
                         <a
                             href="https://github.com/sejin-P"
-                            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+                            className="bg-black hover:bg-white hover:text-black border border-black inset-y-0 right-0 text-white font-bold py-3 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
                         >
                             Go to sejin-P github
                         </a>
